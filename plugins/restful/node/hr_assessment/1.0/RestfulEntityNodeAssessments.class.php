0bd5586e75f8017238a04a0b70d528c2220ab39d
Guillaume Viguier <guillaume@viguierjust.com>
2015-01-05 16:28:24 +0100

    API - Added assessments

---
+++ plugins/restful/node/hr_assessment/1.0/RestfulEntityNodeAssessments.class.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @file
+ * Contains \RestfulEntityNodeAssessments.
+ */
+
+class RestfulEntityNodeAssessments extends \RestfulEntityBaseNode {
+
+  /**
+   * Overrides RestfulEntityBaseNode::addExtraInfoToQuery()
+   * 
+   * Adds proper query tags
+   */
+  protected function addExtraInfoToQuery($query) {
+    parent::addExtraInfoToQuery($query);
+    $filters = $this->parseRequestForListFilter();
+    if (!empty($filters)) {
+      foreach ($query->tags as $i => $tag) {
+        if ($tag == 'node_access') {
+          unset($query->tags[$i]);
+        }
+      }
+      $query->addTag('entity_field_access');
+    }
+  }
+
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['bundles'] = array(
+      'property' => 'field_bundles',
+      'resource' => array(
+        'hr_bundle' => 'bundles',
+      ),
+    );
+
+    $public_fields['organizations'] = array(
+      'property' => 'field_organizations',
+      'resource' => array(
+        'hr_organization' => 'organizations',
+      ),
+    );
+
+    $public_fields['participating_organizations'] = array(
+      'property' => 'field_organizations2',
+      'resource' => array(
+        'hr_organization' => 'organizations',
+      ),
+    );
+
+    $public_fields['locations'] = array(
+      'property' => 'field_locations',
+      'resource' => array(
+        'hr_location' => 'locations',
+      ),
+    );
+
+    $public_fields['other_location'] = array(
+      'property' => 'field_asst_other_location',
+      'subproperty' => 'value',
+    );
+
+    /*$public_fields['address'] = array(
+      'property' => 'field_address',
+    );
+
+    $public_fields['phones'] = array(
+      'property' => 'field_phones',
+    );
+
+    $public_fields['email'] = array(
+      'property' => 'field_email',
+    );
+
+    $public_fields['operation'] = array(
+      'property' => 'og_group_ref',
+      'resource' => array(
+        'hr_operation' => 'operations',
+      ),
+    );*/
+
+    return $public_fields;
+  }
+
+}
